151442f1f906a7d4d798aa849b429976ab6b82f5
obragaa <f.braga.duarte02@gmail.com>
2020-09-24 19:53:07 -0300

    Mockei a API e consumi ela.

--- src/main.js
+++ src/main.js
@@ -1,26 +1,17 @@
-var cep = document.querySelector("#cep")
-
-const showData = (result)=>{
-    for(const campo in result){
-        if(document.querySelector("#"+campo)){
-            document.querySelector("#"+campo).value = result[campo]
-        }
-    }
+function buscaCep() { 
+    let inputCep = document.querySelector('input\[name=id\]'); 
+    let id = inputCep.value.replace('-', '');
+    let url = 'http://localhost:3000/fakeapi/' + id; 
+    let xhr = new XMLHttpRequest(); xhr.open('GET', url, true); 
+    xhr.onreadystatechange = function() { 
+    if (xhr.readyState == 4) { 
+    if (xhr.status = 200) preencheCampos(JSON.parse(xhr.responseText)); } } 
+    xhr.send(); 
 }
 
-
-
-cep.addEventListener("blur",(e)=>{
-    let search = cep.value.replace("-","")
-    const options = {
-        method: 'GET',
-        mode: 'cors',
-        cache: 'default'
-    }
-
-    fetch(`https://viacep.com.br/ws/${search}/json/`, options)
-    .then(response =>{ response.json()
-        .then( data => showData(data))
-    })
-    .catch(e => console.log('Deu Erro: '+ e,message))
-})
+function preencheCampos(json) { 
+    document.querySelector('input[name=endereco]').value = json.logradouro;
+    document.querySelector('input[name=bairro]').value = json.bairro; 
+    document.querySelector('input[name=cidade]').value = json.localidade; 
+    document.querySelector('input[name=estado]').value = json.uf; 
+}
